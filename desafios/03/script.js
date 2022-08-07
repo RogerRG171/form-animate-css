ac29a02746910ecfa4258b1a70e1ab85fa55cfc2
Roger171 <roger.goulart.nascimento@gmail.com>
2022-08-07 14:07:21 -0400

    create challenge three btn animation down js

--- desafios/03/script.js
+++ desafios/03/script.js
@@ -1,7 +1,9 @@
 const btnLogin = document.querySelector(".btn-login");
 const form = document.querySelector("form");
 
-btnLogin.addEventListener("click", event => {
-  event.preventDefault();
-  form.classList.add("form-hide");
+btnLogin.addEventListener("click", (event)=>{
+
+    event.preventDefault();// impede a ação padrão do botão de submit (enviar)
+    form.classList.add("form-hide");
+
 });
